83dd945b5dd53c1586e7d7d75eaa996fe2905aa9
Ruslan Kryukov <ruslan.kryukov@ru.ibm.com>
2017-05-11 20:49:04 +0300

    replace influxdb to service

--- src/main/java/org/blockchain_monitoring/config/InfluxDBConfig.java
+++ src/main/java/org/blockchain_monitoring/config/InfluxDBConfig.java
@@ -4,11 +4,16 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.blockchain_monitoring.api.InfluxService;
 import org.blockchain_monitoring.api.InfluxServiceImpl;
+import org.blockchain_monitoring.api.ResultAdapter;
+import org.blockchain_monitoring.api.annotation.invoke.InvokeMonitoring;
 import org.blockchain_monitoring.model.MonitoringParams;
+import org.influxdb.dto.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
 
 @Configuration
 @ComponentScan("org.blockchain_monitoring")
@@ -30,5 +35,15 @@ public class InfluxDBConfig {
         ObjectMapper m = new ObjectMapper();
         m.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         return m;
+    }
+
+    @Bean
+    public ResultAdapter resultAdapter() {
+        return new ResultAdapter() {
+            @Override
+            public Optional<Point> apply(Object o, InvokeMonitoring invokeMonitoring) {
+                return Optional.empty();
+            }
+        };
     }
 }
